4d6d79778a1e69badfa62cb9daecd065b22a64fb
Vitoria <vick_santos_vick@hotmail.com>
2022-09-17 17:35:41 -0300

    #1 - Adicionado a paleta de cores que serão usadas no projeto

---
+++ lib/presentation/core/paleta_de_cores.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PaletaDeCores {
+  static Color branco = const Color(0xffFEFDFD);
+  static Color azulClaro = const Color(0xFFEDF7F9);
+  static Color verdeClaro = const Color(0xFF84C5BF);
+  static Color verdeEscuro = const Color(0xFF016D77);
+}
