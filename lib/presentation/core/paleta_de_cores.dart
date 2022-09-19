e45565e43f03f13b7f883ac2e9c3c6626c6e680f
Vitoria <vick_santos_vick@hotmail.com>
2022-09-19 00:19:09 -0300

    #1 - Adicionando mais cores

--- lib/presentation/core/paleta_de_cores.dart
+++ lib/presentation/core/paleta_de_cores.dart
@@ -5,4 +5,7 @@ class PaletaDeCores {
   static Color azulClaro = const Color(0xFFEDF7F9);
   static Color verdeClaro = const Color(0xFF84C5BF);
   static Color verdeEscuro = const Color(0xFF016D77);
+  static Color preto = const Color(0xFF041E21);
+  static Color amarelo = const Color(0xFFF6C138);
+  static Color vermelho = const Color(0xFFB51717);
 }
