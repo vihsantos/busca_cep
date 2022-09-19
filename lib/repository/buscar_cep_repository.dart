b1741e61ce0177436f24a00f4bc86c4ac7728a37
Vitoria <vick_santos_vick@hotmail.com>
2022-09-19 01:23:19 -0300

    #1 - [REF] Criação da repository de buscar Cep

---
+++ lib/repository/buscar_cep_repository.dart
@@ -0,0 +1,33 @@
+import 'dart:developer';
+
+import 'package:busca_cep/models/endereco.dart';
+import 'package:http/http.dart' as http;
+
+import '../utils/error/cep_exception_imp.dart';
+
+class BuscarCepRepository {
+  Future<Endereco>? buscarCep(String cep) async {
+    String formatCep = cep.replaceAll(RegExp('[-]'), '');
+
+    var url = "https://www.cepaberto.com/api/v3/cep?cep=$formatCep";
+    var header = {
+      'Authorization': 'Token token=3cce4f4c32f8a837c9d323325b961f1b'
+    };
+
+    var response = await http.get(Uri.parse(url), headers: header);
+
+    if (response.statusCode == 200) {
+      if (response.body == '{}') {
+        throw CepExceptionImp(message: 'Cep não encontrado', statusCode: 404);
+      }
+
+      log(response.body);
+
+      Endereco endereco = Endereco.fromJson(response.body);
+      return endereco;
+    }
+
+    throw CepExceptionImp(
+        message: response.reasonPhrase!, statusCode: response.statusCode);
+  }
+}
