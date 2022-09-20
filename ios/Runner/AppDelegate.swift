2a6046ca29f035021b4eb4969059eec1dab4bd24
Vitoria <vick_santos_vick@hotmail.com>
2022-09-19 23:49:56 -0300

    #1 - Alterações por conta do google_maps

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAR-Ls2m32LZ2Yz1M7Tn9HGi5_jdI9uwlQ")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
