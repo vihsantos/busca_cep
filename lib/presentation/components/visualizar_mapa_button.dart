972114b36d1ac436562a1a40c0b3326dbff91907
Vitoria <vick_santos_vick@hotmail.com>
2022-11-01 22:31:51 -0300

    #1  - Alterando o tipo do botão para visualizar o mapa

--- lib/presentation/components/visualizar_mapa_button.dart
+++ lib/presentation/components/visualizar_mapa_button.dart
@@ -10,16 +10,11 @@ class VisualizarMapaButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return ElevatedButton(
-        style: ElevatedButton.styleFrom(
-            maximumSize: Size(size.width * 0.6, size.height * 0.06)),
+    return TextButton(
         onPressed: function as void Function()?,
         child: const Text(
           "Clique aqui \npara visualizar o mapa",
-          maxLines: 2,
           textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 12.5),
         ));
   }
 }
